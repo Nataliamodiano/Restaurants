9f52d2bf20c1086b542a80a8cbc2eb75460292aa
natalia <nataliamodiano@gmail.com>
2015-12-23 16:06:19 -0800

    Added google maps API, #1

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ submit.addEventListener('click', getApi, false);
 
 function getApi(e) {
   e.preventDefault();
+  //get search box values
   var find = document.getElementById('find').value;
   var category = document.getElementById('category').value || 'food';
   var location = document.getElementById('location').value;
   var radius = document.getElementById('radius').value || '16000';
   var sort = document.getElementById('sort').value || '0';
 
+  //clear previous search results 
   while (result.firstChild) {
     result.removeChild(result.firstChild);
   }
@@ -19,6 +21,8 @@ function getApi(e) {
       var response = JSON.parse(xhr.responseText);
       console.log(response);
       var name = response.businesses;
+
+      //loop through all names in the yelp array
       for (i = 0; i < name.length; i++){
         var result = document.getElementById('result');
         var item = document.createElement('li');
@@ -27,15 +31,20 @@ function getApi(e) {
         var image = document.createElement('img');
         image.setAttribute('src', name[i].rating_img_url);
 
+        //get coordinates
+        var latLong = name[i].location.coordinate;
+        console.log(latLong);
+
         title.textContent = name[i].name;
         paragraph.textContent = ['This restaurant has a rating of ', name[i].rating, 'and a review count of ', name[i].review_count, '.'].join(' ');
 
+        //append results to the page
         result.appendChild(item);
         item.appendChild(title);
         item.appendChild(paragraph);
         title.appendChild(image);
       };
-    } else {alert('broken');}
+    } else {alert('Make sure you entered a Find and Location value');}
   };
 
   xhr.open('GET', 'http://localhost:8080/yelp-api/' + find + '/' + category + '/' + location + '/' + radius + '/' + sort, true);
