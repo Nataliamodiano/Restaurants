58c6047520ceb9853a28931cb24e6a3f1dc12e93
natalia <nataliamodiano@gmail.com>
2015-12-31 17:50:33 -0800

    Added marker of first location to map, #1

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 var submit = document.getElementById('submit');
 submit.addEventListener('click', getApi, false);
 
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: {lat: 34.052235, lng: -118.243683},
+    scrollwheel: false
+  });
+  var geocoder = new google.maps.Geocoder();
+  document.getElementById('submit').addEventListener('click', function() {
+    geocodeAddress(geocoder, map);
+  });
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('location').value;
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      resultsMap.setCenter(results[0].geometry.location);
+      // var marker = new google.maps.Marker({
+      //   map: resultsMap,
+      //   position: results[0].geometry.location
+      // });
+      console.log(address);
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+
 function getApi(e) {
   e.preventDefault();
   //get search box values
@@ -22,6 +51,23 @@ function getApi(e) {
       console.log(response);
       var name = response.businesses;
 
+
+      var address = document.getElementById('location').value;
+      geocoder.geocode({'address': address}, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+          resultsMap.setCenter(results[0].geometry.location);
+          // var marker = new google.maps.Marker({
+          //   map: resultsMap,
+          //   position: results[0].geometry.location
+          }
+        });
+
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: results[0].geometry.location,
+        scrollwheel: false
+      });
+
       //loop through all names in the yelp array
       for (i = 0; i < name.length; i++){
         var result = document.getElementById('result');
@@ -31,23 +77,32 @@ function getApi(e) {
         var image = document.createElement('img');
         image.setAttribute('src', name[i].rating_img_url);
 
-        //get coordinates
-        var latLong = name[i].location.coordinate;
-        console.log(latLong);
-
         title.textContent = name[i].name;
-        paragraph.textContent = ['This restaurant has a rating of ', name[i].rating, 'and a review count of ', name[i].review_count, '.'].join(' ');
+        paragraph.textContent = ['This restaurant has a rating of ', name[i].rating, ' stars and a review count of ', name[i].review_count, '.'].join('');
 
         //append results to the page
         result.appendChild(item);
         item.appendChild(title);
         item.appendChild(paragraph);
         title.appendChild(image);
-      };
-    } else {alert('Make sure you entered a Find and Location value');}
-  };
+
+        //get coordinates
+        var latitude = name[i].location.coordinate.latitude;
+        var longitude = name[i].location.coordinate.longitude;
+        var latLong = {lat: latitude, lng: longitude};
+        console.log(latLong);
+
+        var marker = new google.maps.Marker({
+          position: latLong,
+          map: map,
+          title: 'hi'
+        });
+
+      }
+    } else {alert('Make sure you entered Find and Location values');}
+  }
 
   xhr.open('GET', 'http://localhost:8080/yelp-api/' + find + '/' + category + '/' + location + '/' + radius + '/' + sort, true);
   xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
   xhr.send(location);
-};
+}
