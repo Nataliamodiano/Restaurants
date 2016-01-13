dd6b4a283c78e1c11240395dd1326913905996a4
natalia <nataliamodiano@gmail.com>
2016-01-13 15:04:47 -0800

    Fixed white space

--- yelp-api.js
+++ yelp-api.js
@@ -3,32 +3,28 @@ var yelpRouter = express.Router();
 var request = require('request');
 var Promise = require('bluebird');
 var yelp = require('node-yelp');
-
 yelpRouter.use(function(req, res, next) {
   console.log(req.originalUrl);
   next();
 });
-
 yelpRouter.get('/:term/:location/:radius/:sort', function(req, res) {
   var myYelp = yelp.createClient({
-      oauth: {
-        consumer_key: 'xP7RInW8iBUfnxnKxFc4VQ',
-        consumer_secret: '4SH_FsYWwiEHoCoXWFnEUWwzxvM',
-        token: 'HrCzYsY-No4hXf4yvn2XbdaU0tjK2fpi',
-        token_secret: 'bo8WpWHcGMpB7PZ6zyU4I4wb5Ss'
-      }
-    }); 
-     
-    myYelp.search(
-      {
-        term: req.params.term,
-        location: req.params.location,
-        limit: '20',
-        sort: req.params.sort,
-        radius_filter: req.params.radius
-      }).then(function (data) {
-        res.send(data);
-      }); 
+    oauth: {
+      consumer_key: 'xP7RInW8iBUfnxnKxFc4VQ',
+      consumer_secret: '4SH_FsYWwiEHoCoXWFnEUWwzxvM',
+      token: 'HrCzYsY-No4hXf4yvn2XbdaU0tjK2fpi',
+      token_secret: 'bo8WpWHcGMpB7PZ6zyU4I4wb5Ss'
+    }
+  });   
+  myYelp.search(
+    {
+      term: req.params.term,
+      location: req.params.location,
+      limit: '20',
+      sort: req.params.sort,
+      radius_filter: req.params.radius
+  }).then(function (data) {
+    res.send(data);
+  }); 
 });
-
 module.exports = yelpRouter;
