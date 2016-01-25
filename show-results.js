29456160ea5afbde10d1bddb45a15508fa6664ac
natalia <nataliamodiano@gmail.com>
2016-01-25 15:17:51 -0800

    Moved map back next to search result list, added tab to view my list, removed smooth scroll, added new screenshots

--- show-results.js
+++ show-results.js
@@ -8,17 +8,14 @@ submit.addEventListener('click', showResults, false);
 function showResults() {
   resultList.setAttribute('id', '');
   lists.setAttribute('id', '');
-  manualList.setAttribute('class', 'col-sm-5');
-  manualList.setAttribute('id', '');
 }
 var addRestaurant = document.getElementById('add-restaurant');
-addRestaurant.addEventListener('click', showMyResults, false);
-function showMyResults() {
-  manualList.setAttribute('id', '');
-  lists.setAttribute('id', '');
-  if (resultList.id === '') {
-    manualList.setAttribute('class', 'col-sm-5');
-  } if (resultList.id === 'result-list'){
-    manualList.setAttribute('class', 'center-block');
-  }
-}
+addRestaurant.addEventListener('click', showResults, false);
+
+// tabs functionality
+$('#myTabs a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+$('#result').tab('show')
+$('#restaurant-list').tab('show')
